0ac3154b5a3cb594b16da90d29b379883db314f4
Dyyo <34946258+Dyyo@users.noreply.github.com>
2023-10-24 06:05:55 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,61 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: 'HTML5',
+            level: 85
+        },
+        {
+            name: 'CSS3',
+            level: 85
+        },
+        {
+            name: 'JavaScript',
+            level: 70
+        },
+        {
+            name: 'PHP',
+            level: 70
+        },
+        {
+            name: 'MySQL',
+            level: 70
+        },
+        {
+            name: 'C#',
+            level: 60
+        },
+        {
+            name: 'ASP.NET',
+            level: 60
+        },
+        {
+            name: 'Java',
+            level: 60
+        },
+        {
+            name: 'C++',
+            level: 50
+        },
+        {
+            name: 'Python',
+            level: 50
+        },
+        {
+            name: 'C',
+            level: 50
+        },
+        {
+            name: 'Ruby',
+            level: 50
+        },
+        {
+            name: 'Android',
+            level: 50
+        },
+        {
+            name: 'iOS',
+            level: 50
+        }
+    ];
+    return skills;
+}
